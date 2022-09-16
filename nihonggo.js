0a46dfd566aa83151f27f6d326fa184fc08c21ed
unknown <erskine.duenas@gmail.com>
2022-09-16 09:30:16 +0800

    update

--- nihonggo.js
+++ nihonggo.js
@@ -1,3 +1,14 @@
-// Nihonggo notes
+//  Nihonggo notes
 
-Nice notes
+// Lesson # 2
+
+// - a vowel, e.g. あ (a)
+// - made up of one consonant and one vowel, e.g. か (ka)
+// - ん (n), which is a consonant sound on its own.
+// Now you know the possible sounds that make up a Japanese *character*, but let's look at
+// ALL the sounds in Japanese collectively.
+// You already know roughly 80% of ALL sounds in Japanese. That means that if you were to
+// simply imitate a Japanese speaker, your pronunciation would be correct roughly 80% of
+// the time!
+// Let's consider the word わさび (wasabi).
+// "W" ,"S", and "B" sounds in this Japanese word are identical to English.
