fe0ab15d37c614b481bd09a46ffad546e2dfe488
unknown <erskine.duenas@gmail.com>
2022-09-23 20:53:26 +0800

    udpate

--- nihonggo.js
+++ nihonggo.js
@@ -1,32 +1,35 @@
-//  Nihonggo notes
-/////////////////// Section 1
-// Lesson # 2
-// -Japanese is a 'mora-timed' language; syllables are pronounced equally. 
-// -Japanese characters can represent a vowel, a consonant and a vowel, or a single consonant sound 
-// -Nearly all sounds in Japanese are identical to English. 
-// -Only a handful of new sounds to learn.
+ /* 
+Nihonggo notes
+//////////////// Section 1
+Lesson # 2
+-Japanese is a 'mora-timed' language; syllables are pronounced equally. 
+-Japanese characters can represent a vowel, a consonant and a vowel, or a single consonant sound 
+-Nearly all sounds in Japanese are identical to English. 
+-Only a handful of new sounds to learn.
 
-// Lesson # 3
-// Word Order; subject, object, verb, or "SOV".
-// Topic-prominent language; topic is the focus.
-// Usually omits the subject.
-// Create basic sentences; object + verb.
+Lesson # 3
+Word Order; subject, object, verb, or "SOV".
+Topic-prominent language; topic is the focus.
+Usually omits the subject.
+Create basic sentences; object + verb.
 
-// Lesson #4 
-// -3 different scripts used to write Japanese.
-// -Hiragana is used to write functional words and particles.
-// -Katakana is used to write foreign words and names. 
-// -Kanji us used to write native Japanese words, or words of Chinese origin.
+Lesson #4 
+-3 different scripts used to write Japanese.
+-Hiragana is used to write functional words and particles.
+-Katakana is used to write foreign words and names. 
+-Kanji us used to write native Japanese words, or words of Chinese origin.
 
-// Lesson #5
-// arigato = Thank you
-// sumimasen = excuse me/ I'm sorry
-// wa doko desu ka = Where is ...?
-// Lesson #6
+Lesson #5
+arigato = Thank you
+sumimasen = excuse me/ I'm sorry
+wa doko desu ka = Where is ...?
+Lesson #6
 
-/////////////////// Section 2
-/////////////////// Section 3
-/////////////////// Section 4
-/////////////////// Section 5
-/////////////////// Section 6
-/////////////////// Section 7
+//////////////// Section 2
+//////////////// Section 3
+//////////////// Section 4
+//////////////// Section 5
+//////////////// Section 6
+//////////////// Section 7
+
+*/
